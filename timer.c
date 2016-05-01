a966cb3d143335858b5b914ed47095cdaefe76a6
Dennis Law <cmlaw1993@gmail.com>
2016-05-01 19:25:36 +0800

    Added interrupt for Timer
    Interrupt functionality has been added for the Timer driver.

--- timer.c
+++ timer.c
@@ -10,8 +10,11 @@
  *  @date April 2016
  */
 
-#include <stdbool.h>
+#include <stdio.h>
 #include "timer.h"
+
+void (*timer6_callback)(void) = NULL;
+void (*timer7_callback)(void) = NULL;
 
 /** @brief Writes to the counter register
  *	@param tim The timer to configure.
@@ -47,13 +50,46 @@ int timer_write_prescaler(TIM_TypeDef *tim, uint16_t val)
 	return 0;
 }
 
+/** @brief Enables Timer interrupt
+ *	@tim The timer to configure.
+ *	@returns 0 if successful and -1 if otherwise.
+ *
+ *	@note The timer needs to be configured to enable interrupt in timer_init in
+ *	order for this to work.
+ */
+int timer_enable_interrupt(TIM_TypeDef *tim)
+{
+	if ((tim != TIM6) && (tim != TIM7))
+	return -1;
+	
+	tim->SR	  &= ~(TIM_SR_UIF); /* Clear interrupt flag */
+	tim->DIER |= TIM_DIER_UIE;	/* Enable interrupt */
+	
+	return 0;
+}
+
+/** @brief Disables Timer interrupt
+ *	@tim The timer to configure.
+ *	@returns 0 if successful and -1 if otherwise.
+ */
+int timer_disable_interrupt(TIM_TypeDef *tim)
+{
+	if ((tim != TIM6) && (tim != TIM7))
+	return -1;
+	
+	tim->SR	  &= ~(TIM_SR_UIF); 	/* Clear interrupt flag */
+	tim->DIER &= ~(TIM_DIER_UIE);	/* Disable interrupt */
+	
+	return 0;
+}
+
 /** @brief Initializes a basic Timer
  *	@param tim The timer to be initialized. Its value can be TIM6 or TIM7
  *
  *	A basic timer is initialized for the puspose of being a triggering source
  *	for DAC.
  */
-int timer_init(TIM_TypeDef *tim)
+int timer_init(TIM_TypeDef *tim, bool interrupt, void (*callback)(void))
 {
 	if ((tim != TIM6) & (tim != TIM7))
 		return -1;
@@ -78,5 +114,43 @@ int timer_init(TIM_TypeDef *tim)
 	/* Enable update DMA request */
 	tim->DIER |= TIM_DIER_UDE;
 	
+	/* Enable interrupt */
+	if (interrupt && (tim == TIM6)) {
+		if (callback != NULL) {
+			NVIC_EnableIRQ(TIM6_DAC_IRQn);
+			timer6_callback = callback;
+		} else {
+			return -1;
+		}
+	} else if (interrupt && (tim == TIM7)) {
+		if (callback != NULL) {
+			NVIC_EnableIRQ(TIM7_IRQn);
+			timer7_callback = callback;	
+		} else {
+			return -1;
+		}
+	}
+	
 	return 0;
 }
+
+/** @brief IRQ Handler for Timer 6
+ */
+void TIM6_DAC_IRQHandler(void)
+{
+	TIM6->SR &= ~(TIM_SR_UIF);
+
+	if (timer6_callback)
+		timer6_callback();
+}
+
+/** @brief IRQ Handler for Timer 7
+ */
+void TIM7_IRQHandler(void)
+{
+	TIM7->SR &= ~(TIM_SR_UIF);
+
+	if (timer7_callback)
+		timer7_callback();
+}
+
--- timer.h
+++ timer.h
@@ -8,10 +8,15 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <stdbool.h>
 #include "stm32f0xx.h"
 
 int timer_write_counter(TIM_TypeDef *tim, uint16_t val);
 int timer_write_prescaler(TIM_TypeDef *tim, uint16_t val);
-int timer_init(TIM_TypeDef *tim);
+
+int timer_disable_interrupt(TIM_TypeDef *tim);
+int timer_enable_interrupt(TIM_TypeDef *tim);
+
+int timer_init(TIM_TypeDef *tim, bool interrupt, void (*callback)(void));
 
 #endif	/* TIMER_H */
