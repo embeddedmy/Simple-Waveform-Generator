d3f059d633379abb9ff87fa086bf4fe37a5dd314
Dennis Law <cmlaw1993@gmail.com>
2016-05-04 00:32:30 +0800

    Added files for DMA driver
    Added basic .c and .h files for the DMA driver.

---
+++ dma.c
@@ -0,0 +1,8 @@
+
+/** @file dma.c
+ *  @brief DMA driver
+ *  @author Dennis Law
+ *  @date May 2016
+ */
+
+#include "dma.h"
---
+++ dma.h
@@ -0,0 +1,13 @@
+
+/** @file dma.h
+ *  @brief DMA driver include file
+ *  @author Dennis Law
+ *  @date May 2016
+ */
+
+#ifndef DMA_H
+#define DMA_H
+
+#include "stm32f0xx.h"
+
+#endif	/* DMA_H */
